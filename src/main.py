e6f750e0c95711c1a0ffbfe059460342c32a040a
Steven Hicks <steven@simula.no>
2023-05-08 14:09:46 +0200

    Define agents using a config file.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from typing import List
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (
@@ -9,13 +10,31 @@ from langchain.schema import (
 )
 
 
+class Memory:
+    def __init__(self) -> None:
+        self.memory = []
+
+    def store(self, item):
+        self.memory.append(item)
+
+    def get_memory(self):
+        return SystemMessage(
+            content=".".join([memory.content for memory in self.memory])
+        )
+
+
 class Agent:
     def __init__(
-        self, system_message: BaseMessage, model: ChatOpenAI, name: str
+        self,
+        system_message: BaseMessage,
+        model: ChatOpenAI,
+        name: str,
+        memory: Memory = None,
     ) -> None:
         self.system_message = system_message
         self.model = model
         self.name = name
+        self.memory = memory
         self.init_messages()
 
     def reset(self) -> None:
@@ -32,12 +51,14 @@ class Agent:
         messages = self.update_messages(input_message)
         output_message = self.model(messages)
         self.update_messages(output_message)
+        self.memory.store(output_message)
         return output_message
 
 
 class Simulation:
-    def __init__(self, agents: List[Agent]) -> None:
+    def __init__(self, agents: List[Agent], god: Agent = None) -> None:
         self.agents = agents
+        self.god = god
 
     def run_simulation(self, chat_turn_limit: int = 30) -> None:
         self.reset_agents()
@@ -45,68 +66,68 @@ class Simulation:
         n = 0
         while n < chat_turn_limit:
             n += 1
+            gods_word = HumanMessage(
+                content=self.god.step(self.god.memory.get_memory()).content
+            )
+            print("[GOD]: Now speak %s" % gods_word.content)
+
+            next_speaker = gods_word.content
+
             for agent in self.agents:
-                msg = HumanMessage(content=agent.step(msg).content)
-                print(f"{agent.name}: {msg.content}\n")
+                if agent.name == next_speaker:
+                    msg = HumanMessage(content=agent.step(msg).content)
+                    print(f"[{agent.name}]: {msg.content}\n")
+                    break
 
     def reset_agents(self) -> None:
         for agent in self.agents:
             agent.reset()
 
 
-def create_agents(task: str, temperature: float = 0.2) -> List[Agent]:
-    teacher_system_message = SystemMessage(
-        content=f"""Never forget you are a Teacher and I am a Student. Never flip roles!
-        We share a common interest in having a conversation.
-        You must help me to keep the conversation going.
-        Here is the topic of our conversation: {task} and my perspective I am a very smart math teacher. Never forget our topic!
-
-        You can ask and answer questions depending on the context of the conversation."""
-    )
-
-    student_system_message = SystemMessage(
-        content=f"""Never forget you are a Student and I am a Teacher. Never flip roles!.
-        We share a common interest in having a conversation.
-        I must help you to keep the conversation going.
-        Here is the topic of our conversation: {task} and my perspective I am a very dumb student who has problems to understand simple arithmatic. Never forget our topic!
-
-        I can ask and answer questions depending on the context of the conversation."""
-    )
-
-    return [
-        Agent(teacher_system_message, ChatOpenAI(temperature=temperature), "Teacher"),
-        Agent(student_system_message, ChatOpenAI(temperature=temperature), "Student"),
-    ]
-
-
 def main() -> None:
     parser = argparse.ArgumentParser(
         description="Run a simulation with the given task."
     )
+
     parser.add_argument(
-        "--task",
+        "--config",
         type=str,
-        default="Make the student understand why 2 + 2 equals 4",
-        help="The task for the simulation.",
-    )
-    parser.add_argument(
-        "--temperature",
-        type=float,
-        default=0.2,
-        help="The temperature used for the ChatOpenAI model.",
-    )
-    parser.add_argument(
-        "--chat_turn_limit",
-        type=int,
-        default=30,
-        help="The maximum number of chat turns.",
+        default="config.json",
+        help="The number of agents for the simulation.",
     )
 
     args = parser.parse_args()
 
-    agents = create_agents(task=args.task, temperature=args.temperature)
-    simulation = Simulation(agents)
-    simulation.run_simulation(chat_turn_limit=args.chat_turn_limit)
+    with open(args.config, "r") as f:
+        config = json.load(f)
+
+    agents = []
+    shared_memory = Memory()
+
+    god = Agent(
+        SystemMessage(content=config["god"]["system_message"]),
+        ChatOpenAI(
+            temperature=config["god"]["temperature"],
+            model_name=config["god"]["modelName"],
+        ),
+        memory=shared_memory,
+        name=config["god"]["name"],
+    )
+
+    for agent_config in config["agents"]:
+        agent = Agent(
+            SystemMessage(content=agent_config["system_message"]),
+            ChatOpenAI(
+                temperature=agent_config["temperature"],
+                model_name=agent_config["modelName"],
+            ),
+            memory=shared_memory,
+            name=agent_config["name"],
+        )
+        agents.append(agent)
+
+    simulation = Simulation(agents=agents, god=god)
+    simulation.run_simulation(chat_turn_limit=config["turnLimit"])
 
 
 if __name__ == "__main__":
